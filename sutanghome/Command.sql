47383dc3707485043136f1f4f83a4becfd596431
Aldwin Su <fangyansu@163.com>
2020-02-04 10:44:54 +0800

    Update command of sutanghome

--- sutanghome/Command.sql
+++ sutanghome/Command.sql
@@ -12,15 +12,21 @@ inner join shopping s on p.id = s.paymentid
 inner join user u on p.userid = u.id and u.deleted = 0
 where p.deleted = 0;
 
-#Select all transfer for display.
+/*Select all transfer for display*/
+
+--Select all that I created and paied.
 select p.id, fu.name userName, p.type, t.to_user_id toUserId, tu.name toUser, p.cost, p.medium, p.time, p.description, p.creatorid, p.created, p.updated
 from (select * from payment where userid = 17 or creatorid = 17) p
 join transfer t on p.id = t.paymentid
 inner join user fu on p.userid = fu.id and fu.deleted = 0
 inner join user tu on t.to_user_id = tu.id and tu.deleted = 0
 union
+
+--Select all that transfered to me.
 select p.id, fu.name userName, p.type, t.to_user_id toUserId, tu.name toUser, p.cost, p.medium, p.time, p.description, p.creatorid, p.created, p.updated
 from payment p
 join (select * from transfer where to_user_id = 17) t on p.id = t.paymentid
 inner join user fu on p.userid = fu.id and fu.deleted = 0
 inner join user tu on t.to_user_id = tu.id and tu.deleted = 0;
+
+/*Select all transfer for display end*/
